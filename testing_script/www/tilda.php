be4e85700976a97895477aff5e66e0e2c66a3035
ilx-prof <38192754+ilx-prof@users.noreply.github.com>
2021-11-26 00:50:40 +0300

    Add files via upload

--- testing_script/www/tilda.php
+++ testing_script/www/tilda.php
@@ -50,7 +50,7 @@
 			$r++;
 		}
 	}
-
+print "<br> <br> массив 5х7 заполненый случано от 1 до 1000<br>";
 $l=7;
 $s=5;
 $a = array_map(function($n) { return rand(1,1000); }, range(0, $l*$s-1));
@@ -65,20 +65,39 @@ array_unshift($a, null);
 $a = call_user_func_array('array_map', $a);
 $st=array_map(function($n) { return array_sum($n); },$a);
 print_r ($st);
-
-$ch = curl_init('https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=' . $_SERVER['REMOTE_ADDR']);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Token f7cc45e47b3f214723acb106f9abb8660b993d57'));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HEADER, false);
-$res = curl_exec($ch);
-curl_close($ch);
+function ip_data($ip = 0)//Функция вернет массив с данными 
+{	
+	if ($ip==0)
+	{
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+//	print $ip;
+	$ch = curl_init('https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=' . $ip);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Token f7cc45e47b3f214723acb106f9abb8660b993d57'));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	$res = curl_exec($ch);
+	curl_close($ch);
+	$res = json_decode($res, true);
+	return $res;
+}
  
-$res = json_decode($res, true);
-//print_r ($res);
-print ("<br><H1>Вы находитесь в ".$res["location"]["data"]["city_with_type"]." ".$res["location"]["data"]["region_with_type"]."</h1>");
-print ('<a href="https://www.google.com/maps/search/"'.$res["location"]["data"]["geo_lat"].",".$res["location"]["data"]["geo_lon"].'">Гдето тут на гугл карте</a>')
 
+$switch  = ["Москва" => "8800[филиалМосква]","Санкт-Петербург" => "8800[филиалПитер]","Новосибирск" => "8800[филиалНовосибирск]"];
+$res=ip_data ();
+//print_r ($res);
+print ("<br>Вы находитесь в ".$res["location"]["data"]["city_with_type"]."<br>");
+//print ('<a href="https://www.google.com/maps/search/"'.$res["location"]["data"]["geo_lat"].",".$res["location"]["data"]["geo_lon"].'">Гдето тут на гугл карте</a><br>');
+if (isset ($switch[$res["location"]["data"]["city"]]))
+{
+	$tel = $switch[$res["location"]["data"]["city"]];
+}
+else{
+	$tel = $switch["Москва"];
+}
+
+print ("<br><H1>Позвоните по телефону ".$tel." </h1>");
 	?>
 	</body>
 		</html>
